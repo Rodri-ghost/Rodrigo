22491548f463c12f7ff1217a3b349bd63aed5072
Rodri-ghost <Rodrigo.dos.jogos.2022@gmail.com>
2024-10-09 20:04:27 +0100

    Revisão

---
+++ Rodrigo Prog01/Calculadora.py
@@ -0,0 +1,8 @@
+print("Bem vindo a minhinha calculadora")
+
+
+primeiroNumero=int(input("\nDigite o primeeiro número:"))
+segundoNumero=int(input("\nDigite o segundo número:"))
+calculo=primeiroNumero+segundoNumero
+
+print(f"A soma dos valores é igual a:{calculo}")
